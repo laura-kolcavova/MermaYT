df2d40dabacfa96c70dae13ee82f2aba100930a0
Laura Kolčavová <laura.kolcavova@notino.com>
2026-06-05 22:19:02 +0200

    feat: download queue

---
+++ src/MermaYT.Core/DownloadItem.cs
@@ -0,0 +1,27 @@
+﻿namespace MermaYT.Core;
+
+public sealed class DownloadItem
+{
+    public required string Title { get; set; }
+
+    public required string Url { get; set; }
+
+    public required OutputFormat OutputFormat { get; set; }
+
+    public DownloadState DownloadState { get; set; } = DownloadState.Queued;
+
+    public long TotalBytes { get; set; } = -1;
+
+    public long DownloadedBytes { get; set; }
+
+    public float Progress { get; set; }
+
+    public required string? ImageUrl { get; set; }
+
+    public string? ErrorMessage { get; set; }
+
+    public float GetProgress() =>
+        TotalBytes > 0
+            ? (float)DownloadedBytes / TotalBytes
+            : 0;
+}
--- src/MermaYT.Core/OutputProfile.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MermaYT.Core;
-
-public enum OutputProfile
-{
-    MP3 = 1,
-
-    MP4 = 2,
-}
