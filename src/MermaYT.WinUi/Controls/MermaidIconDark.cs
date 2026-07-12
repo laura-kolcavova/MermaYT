436091008a1f4a70177db2d5c3e05f0030600a1b
Laura Kolčavová <laura.kolcavova@notino.com>
2026-07-12 12:33:11 +0200

    fix: update

---
+++ src/MermaYT.WinUi/Controls/MermaidIconDark.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+
+namespace MermaYT.WinUi.Controls;
+
+public sealed partial class MermaidIconDark :
+    Control
+{
+    public static readonly DependencyProperty FillBrushProperty = DependencyProperty.Register(
+        nameof(FillBrush),
+        typeof(Brush),
+        typeof(MermaidIconDark),
+        new PropertyMetadata(default));
+
+    public Brush FillBrush
+    {
+        get => (Brush)GetValue(FillBrushProperty);
+        set => SetValue(FillBrushProperty, value);
+    }
+
+    public MermaidIconDark()
+    {
+        DefaultStyleKey = typeof(MermaidIconDark);
+    }
+}
---
+++ src/MermaYT.WinUi/Controls/MermaidIconLight.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+
+namespace MermaYT.WinUi.Controls;
+
+public sealed partial class MermaidIconLight :
+    Control
+{
+    public static readonly DependencyProperty FillBrushProperty = DependencyProperty.Register(
+        nameof(FillBrush),
+        typeof(Brush),
+        typeof(MermaidIconLight),
+        new PropertyMetadata(default));
+
+    public Brush FillBrush
+    {
+        get => (Brush)GetValue(FillBrushProperty);
+        set => SetValue(FillBrushProperty, value);
+    }
+
+    public MermaidIconLight()
+    {
+        DefaultStyleKey = typeof(MermaidIconLight);
+    }
+}
